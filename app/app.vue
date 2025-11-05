23ce7b9e1dce7ea6e1265ff0bca19bea981c0aca
Jasur Ibraimov <ibraimov0409@mail.ru>
2025-11-05 10:55:16 +0500

    Tl 10 (#50)
    * :sparkles: setup eslint
    
    * :sparkles: setup eslint
    
    * :sparkles: tailwindcss/daisy ui/icons
    
    * :sparkles: tailwindcss/daisy ui/icons

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
-    <div>
-        <NuxtRouteAnnouncer />
-        <NuxtWelcome />
+    <div class="bg-red-500">
+        Hello World <Icon name="tabler:home" />
     </div>
 </template>
