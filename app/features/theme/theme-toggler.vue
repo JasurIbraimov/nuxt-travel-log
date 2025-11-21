58efd8c27cf1e1fc2b45c824f64aa9a5cd579257
Jasur Ibraimov <ibraimov0409@mail.ru>
2025-11-21 11:49:19 +0500

    :sparkles: Applied FSD (#58)

---
+++ app/features/theme/theme-toggler.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { useColorMode } from "#imports";
+import { computed } from "vue";
+
+import AppSwitch from "~/shared/ui/app-switch.vue";
+
+const colorMode = useColorMode();
+
+const isDark = computed({
+    get: () => colorMode.value === "dark",
+    set: (value: boolean) => {
+        colorMode.preference = value ? "dark" : "light";
+    },
+});
+</script>
+
+<template>
+    <AppSwitch
+        v-model="isDark"
+        :icons="{
+            on: { name: 'tabler:sun', size: 24 },
+            off: { name: 'tabler:moon-filled', size: 24 },
+        }"
+    />
+</template>
