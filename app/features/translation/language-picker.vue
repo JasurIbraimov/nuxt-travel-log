30e1fb88e03d8a7d33542812d89392804b778e32
JasurIbraimov <ibraimov0409@mail.ru>
2025-11-21 15:34:40 +0500

    :sparkles: Added burger menu / adaptive on mobile

--- app/features/translation/language-picker.vue
+++ app/features/translation/language-picker.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
 import AppButton from "~/shared/ui/app-button.vue";
 
-const { availableLocales, setLocale } = useI18n();
+const { availableLocales, setLocale, locale } = useI18n();
 </script>
 
 <template>
-    <div class="dropdown">
+    <div class="dropdown dropdown-center">
         <AppButton
-            variant="ghost"
+            variant="accent"
             outline
             :label="$t('buttons.pick_language')"
             :icon="{ name: 'tabler:language', size: 24 }"
         />
-        <ul tabindex="0" class="menu gap-4 dropdown-content bg-base-100 rounded-box z-1 p-4 w-32 shadow-sm">
+        <ul tabindex="0" class="menu gap-4 dropdown-content bg-base-100 rounded-box z-1 p-4 w-32 shadow-lg left-0.5 top-full ">
             <AppButton
                 v-for="lang in availableLocales"
                 :key="lang"
-                outline
+                :outline="locale !== lang"
                 :label="lang"
                 @click="setLocale(lang);"
             />
--- app/widgets/nav/nav-bar.vue
+++ app/widgets/nav/nav-bar.vue
@@ -12,10 +12,20 @@ import LanguagePicker from "~/features/translation/language-picker.vue";
             </NuxtLink>
         </div>
 
-        <div class="navbar-end gap-2">
-            <LanguagePicker />
-            <ThemeToggler />
-            <AuthButton />
+        <div class="navbar-end">
+            <div class="dropdown dropdown-left">
+                <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+                </div>
+                <div
+                    tabindex="-1"
+                    class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 gap-4 w-52 px-4 py-8 shadow-lg top-full"
+                >
+                    <ThemeToggler />
+                    <AuthButton />
+                    <LanguagePicker />
+                </div>
+            </div>
         </div>
     </nav>
 </template>
